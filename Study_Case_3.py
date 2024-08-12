45a5e7bc336fb14b27d77e9274f5a7bae0232e18
Thomas Frade <thomas.frade@usp.br>
2024-08-12 09:00:47 -0300

    Finishing Touches
    Modified dmg_die to dmg_dice
    added multiple die damage roll support

--- Study_Case_3.py
+++ Study_Case_3.py
@@ -8,20 +8,20 @@ class Weapon():
         Current properties (and builder patterns) include:
          - use_attr: The atributes related to weapon usage
          - attr_req: The minimum required level to use the weapon fully
-         - dmg_die: The die to be used in order to roll damage
+         - dmg_dice: The dice to be used in order to roll damage
 
         Current methods include:
-         - atr_roll: handles the die rolling for damage
+         - atr_roll: handles the dice rolling for damage
         """
         self.use_attr = None
         self.attr_req = None
-        self.dmg_die = None
+        self.dmg_dice = []
     
     def set_use_attr(self, *args: str):
         "Builder pattern for weapon attributes"
         for arg in args:
             if type(arg) is not str:  # Error Handling
-                raise TypeError('Parameters must be strings separated by commas')
+                raise TypeError('Parameters must be string objects')
             if arg.upper() not in gl.attributes:
                 raise ValueError('Weapon parameters must be valid Entity attributes')
         
@@ -40,23 +40,27 @@ class Weapon():
         self.attr_req = args
         return self
     
-    def set_dmg_die(self, die: sc_i.Die):
+    def set_dmg_dice(self, *args: sc_i.Die):
         "Builder pattern for damage die"
-        if type(die) is not sc_i.Die:  # Error Handling
-            raise TypeError('Parameter must be Die object')
+        for arg in args:
+            if type(arg) is not sc_i.Die:  # Error Handling
+                raise TypeError('Parameters must be die objects.')
         
-        self.dmg_die = die
+        self.dmg_dice = args
         return self
     
     @classmethod
     def atk_roll(cls, weapon):
-        "Method for rolling attack die."
-        return sc_i.Die.roll(weapon.dmg_die)
+        "Method for rolling attack dice."
+        result = 0
+        for die in weapon.dmg_dice:
+            result += sc_i.Die.roll(die)
+        return result
 
 if __name__ == "__main__":
     stick = Weapon().set_use_attr('Str', 'Vit')
 
     d4 = sc_i.Die().set_num_sides(4)
-    stick.set_dmg_die(d4)
-
-    print(stick.dmg_die)
+    stick.set_dmg_dice(d4, d4, d4, d4)
+    
+    print(Weapon.atk_roll(stick))
