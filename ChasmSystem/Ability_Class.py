7a91f16ee00c6c167b66ee08478c9b77b9e07be1
Thomas Frade <thomas.frade@usp.br>
2024-08-22 15:01:03 -0300

    Executor class implementation
    Replicated a few methods from Fetcher class
    Traced next steps

--- ChasmSystem/Ability_Class.py
+++ ChasmSystem/Ability_Class.py
@@ -5,6 +5,10 @@ class Ability():
         self.parameters: dict = Fetcher() \
             .set_flags(**kwargs) \
             .return_reqs()
+        
+        self.process: None = Executor() \
+            .set_flags(**kwargs) \
+            .return_proc()
     
     def engage(self, ent_data: tuple):
         pass
@@ -55,6 +59,59 @@ class Fetcher():
             }
 
 
+class Executor():
+    def __init__(self) -> None:
+        self.flags: dict[str, tuple]
+
+    def set_flags(self, **kwargs: tuple):
+        "Builder method for flags"
+
+        for arg in kwargs.items():
+            self.check_for_parameter_flag(arg[0])
+        
+        self.flags = kwargs
+        return self
+    
+    def return_proc(self) -> None:
+        pass
+
+    @staticmethod
+    def compose_variables(base_list: list, in_1: int, in_2: int, delete_second_element: bool = False):
+        gl.check_for_type(base_list[in_1], int, message='list element indexed is the wrong type')
+        gl.check_for_type(base_list[in_2], int, message='list element indexed is the wrong type')
+
+        base_list[in_1] = base_list[in_1] * base_list[in_2]
+        if delete_second_element:
+            base_list[in_2] = None
+
+    # Execute Parameter Flags
+    
+    @classmethod
+    def check_for_parameter_flag(cls, element: str, message: str = 'Parameter is not a valid flag') -> None:
+        "Function to confirm if a given string qualifies as a parameter flag"
+
+        gl.check_in_config(element, cls.parameter_flags.keys(), message)    
+
+    parameter_flags = {
+            'FETCH_STATS': None
+            }
+    
+
 if __name__ == '__main__':
     walking = Ability(FETCH_STATS=('Vit', 'Str'))
     print(walking.parameters['FETCH_STATS']({'VIT': 10, 'STR': 15}))
+
+    test_list = ['5', 2]
+    Executor.compose_variables(test_list, 0, 1)
+    print(test_list)
+    Executor.compose_variables(test_list, 0, 1)
+    print(test_list)
+    Executor.compose_variables(test_list, 0, 1)
+    print(test_list)
+
+    # Where i've stopped: Setting how parameters will be given to methods
+
+    # What i must do next:
+    # - Experiment and define how different steps chain with each other
+    # - Figure out how to export the whole process from the Executor class
+    # - Ensure the data structure reliability of the Fetcher output
